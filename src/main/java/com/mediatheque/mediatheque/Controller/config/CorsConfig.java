ee1745d47c5378f6b5e17e2f16f5c6f769830ad0
walidhbabou <walidhbabou@gmail.com>
2024-12-26 23:39:12 +0100

    ff

---
+++ src/main/java/com/mediatheque/mediatheque/Controller/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.mediatheque.mediatheque.Controller.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") // Ajoutez vos origines
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
